909944dc29ff2b82cc934c9d497f0f24983f0f47
BRAHMAJIPANTAM <brahmajipantham90@gmail.com>
2024-08-27 13:58:02 +0530

    Add files via upload

---
+++ Switch2.java
@@ -0,0 +1,33 @@
+package basicpro;
+import java.util.*;
+public class Switch2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		switch(ch){
+			case 'a':
+				System.out.println("vowel");
+				break;
+			case 'e':
+				System.out.println("vowel");
+				break;
+			case 'i':
+				System.out.println("vowel");
+				break;
+			case 'o':
+				System.out.println("vowel");
+				break;
+			case 'u':
+				System.out.println("vowel");
+				break;
+			default:
+				System.out.println("consonent");
+				
+		}
+		
+
+	}
+
+}
