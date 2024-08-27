21d5feb0803967724f2987bc161de748ec89c7e3
BRAHMAJIPANTAM <brahmajipantham90@gmail.com>
2024-08-27 14:19:45 +0530

    Add files via upload

---
+++ basicprograms/src/basicprograms/Switch3.java
@@ -0,0 +1,24 @@
+package basicpro;
+import java.util.*;
+public class Switch3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		switch(n) {
+		case 10:
+			System.out.println("success");
+			break;
+		case 20:
+			System.out.println("success");
+			break;
+		case 30:
+			System.out.println("success");
+			break;
+			default:
+				System.out.println("value between 10,20 and 30");
+		}
+	}
+
+}
