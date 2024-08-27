a0f8ef2047bfb9639e86262a9255330d479c719f
BRAHMAJIPANTAM <brahmajipantham90@gmail.com>
2024-08-27 15:19:35 +0530

    Add files via upload

---
+++ Starpro1.java
@@ -0,0 +1,17 @@
+package basicpro;
+import java.util.*;
+public class Starpro1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc =new Scanner(System.in);
+		//int n=sc.nextInt();
+		for(int i=0;i<=5;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
